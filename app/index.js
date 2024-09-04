c95317a1832206eb2a7d2aaefd75c42ae4f0159d
Noxde <mateo.tesorieri@hotmail.com>
2024-09-04 00:33:08 -0300

    Se revisa la cartelera cada 3 horas automaticamente

--- app/index.js
+++ app/index.js
@@ -1,16 +1,74 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  TextChannel,
+  EmbedBuilder,
+} = require("discord.js");
 const dotenv = require("dotenv");
+const { fetchAdvertises } = require("./commands/cartelera/fetchCartelera");
+const { setTimeout } = require("node:timers/promises");
+
+const cheerio = require("cheerio");
+const guilds = require("./guilds.json");
+const lastPosts = require("./lastMessage.json");
 
 dotenv.config();
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
+client.servers = guilds; // Guardo los servidores en guilds.json
 
-client.once(Events.ClientReady, (readyClient) => {
+client.once(Events.ClientReady, async (readyClient) => {
   console.log(`Bot ${readyClient.user.tag} iniciado`);
+  /**
+   * @type {{canal: TextChannel, materias: [{name: String, id: Number}]}[]}
+   */
+  const servers = await Promise.all(
+    guilds
+      .filter((x) => x.materias.length != 0)
+      .map(async (x) => {
+        const { serverid, canal: canalId, materias } = x;
+        const server = await client.guilds.fetch(serverid);
+        const canal = await server.channels.fetch(canalId);
+
+        return {
+          canal,
+          materias,
+        };
+      })
+  );
+
+  setInterval(async () => {
+    const now = new Date();
+    const hour = now.getHours();
+    if (hour > 0 && hour < 7) return;
+
+    for (let serverInfo of servers) {
+      const { canal, materias } = serverInfo;
+
+      for (let materia of materias) {
+        const lastPost = await fetchAdvertises(materia.id, 1);
+        const isLast = lastPosts[materia.id] === lastPost.mensajes[0].cuerpo;
+        if (isLast) continue;
+
+        // Si hay un mensaje nuevo, actualizar el json
+        lastPosts[materia.id] = lastPost.mensajes[0].cuerpo;
+        fs.writeFileSync(
+          "./app/lastMessage.json",
+          JSON.stringify(lastPosts, null, "\t")
+        );
+        await canal.send({
+          embeds: [makeEmbed(lastPost, materia.id)],
+        });
+        await setTimeout(1000);
+      }
+    }
+  }, 3 * 60 * 60 * 1000); // 3 horas
 });
 
 client.commands = new Collection();
@@ -41,7 +99,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
   if (interaction.isChatInputCommand()) {
     try {
-      await command.execute(interaction);
+      await command.execute(interaction, client);
     } catch (error) {
       console.error(error);
     }
@@ -55,3 +113,46 @@ client.on(Events.InteractionCreate, async (interaction) => {
 });
 
 client.login(process.env.TOKEN);
+
+function makeEmbed(info, id) {
+  const [mensaje] = info.mensajes;
+  const cuerpoTexto = cheerio.load(mensaje.cuerpo).text();
+  let adjuntosTexto = "";
+
+  if (mensaje.adjuntos && mensaje.adjuntos.length > 0) {
+    adjuntosTexto = mensaje.adjuntos
+      .map((adj) => `[${adj.nombre}](${adj.public_path})`)
+      .join("\n");
+  }
+
+  const embed = new EmbedBuilder()
+    .setAuthor({
+      name: `${mensaje.materia}`,
+      iconURL: "https://gestiondocente.info.unlp.edu.ar/favicon.png",
+      url: `https://gestiondocente.info.unlp.edu.ar/cartelera/#form[materia]=&${id}`,
+    })
+    .setTitle(
+      `${mensaje.is_anulado ? `~~${mensaje.titulo}~~` : `${mensaje.titulo}`}`
+    )
+    .setDescription(
+      `${
+        mensaje.is_anulado
+          ? `~~${cuerpoTexto} \n\n ${adjuntosTexto}~~`
+          : `${cuerpoTexto} \n\n ${adjuntosTexto}`
+      }`
+    )
+    .setColor("#a30a16")
+    .setFooter({
+      text: `${
+        mensaje.is_anulado
+          ? ` ${`Publicado por ${mensaje.autor.trim()} el ${
+              mensaje.fecha
+            } y anulado el ${new Date(
+              mensaje.fecha_anulacion
+            ).toLocaleDateString()}`}`
+          : `${`Publicado por ${mensaje.autor.trim()} el ${mensaje.fecha}`}`
+      }`,
+    });
+
+  return embed;
+}
